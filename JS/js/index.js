9874f0b37786cf0b97dad24b347108533dab5226
Javier Lete <jglete@gmail.com>
2019-01-15 08:56:01 +0100

    Operadores

--- JS/js/index.js
+++ JS/js/index.js
@@ -2,6 +2,7 @@
 /*eslint-env browser*/
 /*eslint no-console: off*/
 
+/*
 function saludar() {
     'use strict';
 
@@ -33,3 +34,54 @@ function saludar() {
 }
 
 saludar();
+*/
+
+function operadores() {
+    'use strict';
+    // +, -, *, /
+    console.log(5 % 2); //Resto de la división entera de 5 / 2
+    
+    // console.log(5 ** 2);
+    // 5 elevado a 2
+    // ECMAScript 2016 (7)
+
+    // >, <, >=, <=
+    var x, y;
+    x = 5;
+    y = 7;
+
+    console.log(x, y);
+
+    if (x === y) {
+        console.log('X es igual que Y');
+    } else {
+        console.log('No son iguales');
+    }
+    
+    // ==, ===, !=, !==
+    // === ESTRICTAMENTE IGUAL (incluyendo tipos)
+    // !== ESTRICTAMENTE DESIGUAL (incluyendo tipos)
+    
+    // && (AND), || (OR), ! (NOT)
+    
+    // &, |, ^, ~ a nivel de bits
+    
+    // +=, -=, *=, /= ...
+    x = x + 2;
+    x += 2;
+    
+    // ++, --
+    console.log(x);
+    //console.log(x++); //POSTincremento
+    console.log(x);
+    
+    console.log(y);
+    //console.log(++y); //PREincremento
+    console.log(y);
+    
+    console.log(x > y ? x : y);
+    
+    
+}
+
+operadores();
