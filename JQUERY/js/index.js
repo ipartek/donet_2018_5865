882dab8e40b3c3170f35174161fab40f2bbd613b
Javier Lete <jglete@gmail.com>
2019-01-16 09:10:12 +0100

    Inicio jQuery

---
+++ JQUERY/js/index.js
@@ -0,0 +1,10 @@
+/*global $*/
+/*jslint devel: true, plusplus: true, continue: true*/
+/*eslint-env browser*/
+/*eslint no-console: off*/
+
+$(function () {
+    'use strict';
+    
+    $('h1').text('Cambiado desde jQuery');
+});
