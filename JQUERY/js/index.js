45c3e6f818ff2b947c10d469f5e46c0ce8973f09
Javier Lete <jglete@gmail.com>
2019-01-16 10:04:21 +0100

    Validación del campo email con jQuery

--- JQUERY/js/index.js
+++ JQUERY/js/index.js
@@ -6,7 +6,44 @@
 $(function () {
     'use strict';
     
-    $('h1').text('Cambiado desde jQuery');
+    $('h1,h2').click(function () {
+        $(this).text('Cambiado desde jQuery');
+    });
     
+    //$('input[type=checkbox]')
+    $('#checkbox-password').change(function () {
+        $('#password').prop('type', this.checked ? 'text' : 'password');
+        
+        /*
+        if ($(this).prop('checked')) {
+            $('#password').prop('type', 'text');
+        } else {
+            $('#password').prop('type', 'password');
+        }
+        */
+    });
     
+    $('form').submit(function (e) {
+        var valido = true, $email;
+        
+        e.preventDefault();
+        
+        $email = $('#email');
+        //email = $email[0];
+        
+        if ($email.val().indexOf('hotmail') !== -1) {
+            valido = false;
+            $email.focus().val('');
+            
+            //$('<span>').addClass('error').text('No aceptamos emails públicos').insertAfter('#email');
+            
+            if (!$email.hasClass('error')) {
+                $email.after($('<span class="error">No aceptamos emails públicos</span>')).addClass('error');
+            }
+        }
+        
+        if (valido && confirm('¿Estás seguro de enviar los datos?')) {
+            this.submit();
+        }
+    });
 });
