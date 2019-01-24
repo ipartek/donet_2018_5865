69aa9628e441467b29ac01a3e8aff1a52d431e1d
Javier Lete <jglete@gmail.com>
2019-01-24 14:30:54 +0100

    Versión alfa de comprobación de fechas con momentjs

--- EJERCICIOWEB/js/index.js
+++ EJERCICIOWEB/js/index.js
@@ -1,34 +1,59 @@
-/*global $*/
+/*global $, moment*/
 /*jslint devel: true, plusplus: true, continue: true*/
 /*eslint-env browser*/
 /*eslint no-console: off*/
 
-function extraerCamposUrl(url) {
+var ultimoId = 2;
+
+function esDeEdadLegalParaTrabajar(fechaDeNacimiento) {
     'use strict';
-
-    var datos = [];
-
-    $(url.split('?')[1].split('&')).each(function () {
-        var datoValor = this.split('=');
-        datos[datoValor[0]] = datoValor[1];
-    });
-
-    return datos;
+    
+    var hoy, cumpleMayoriaEdad, cumpleJubilacion;
+    
+    hoy = moment().startOf('day');
+    
+    cumpleMayoriaEdad = moment(fechaDeNacimiento).add(18, 'year');
+    cumpleJubilacion = moment(fechaDeNacimiento).add(65, 'year');
+    
+    return hoy >= cumpleMayoriaEdad && hoy < cumpleJubilacion;
 }
 
-function limpiarFormulario() {
+/*
+function esDeEdadLegalParaTrabajar(fechaDeNacimiento) {
     'use strict';
+    
+    var hoy = new Date(), cumpleMayoriaEdad, cumpleJubilacion;
+    
+    hoy = new Date(hoy.getFullYear(), hoy.getMonth(), hoy.getDate(), 1, 0, 0);
+    
+    cumpleMayoriaEdad = new Date(fechaDeNacimiento);
+    cumpleMayoriaEdad.setFullYear(fechaDeNacimiento.getFullYear() + 18);
+    
+    cumpleJubilacion = new Date(fechaDeNacimiento);
+    cumpleJubilacion.setFullYear(fechaDeNacimiento.getFullYear() + 65);
+    
+    return hoy >= cumpleMayoriaEdad && hoy < cumpleJubilacion;
+}
+*/
+function esDniExtranjero(dni) {
+    'use strict';
+    
+    return isNaN(parseInt(dni[0], 10));
+}
 
-    //var form = document.forms[0];
-
-    //$('#nombre').val('');
-    //form.apellidos.value = '';
-
-    //$('input:not([type=hidden])').val('');
-    $('input').val('');
-    $('select').val('0');
-
-    //form.reset();
+function esDniValido(dni) {
+    'use strict';
+    var numero, letra, letras;
+    
+    letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    
+    numero = dni.substring(0, dni.length - 1);
+    
+    letra = dni[8];
+    
+    numero = numero.replace('X', '0').replace('Y', '1').replace('Z', '2');
+    
+    return letra === letras[numero % 23];
 }
 
 function filaTablaAFormulario(id) {
@@ -54,10 +79,6 @@ function filaTablaAFormulario(id) {
     //$('#id' + id).data('apellidos');
 }
 
-function borrarFilaTabla(id) {
-    'use strict';
-    $('#id' + id).remove();
-}
 
 function modificarBoton(texto, tipo) {
     'use strict';
@@ -66,11 +87,57 @@ function modificarBoton(texto, tipo) {
     $('#btn-enviar').text(texto);
 }
 
+/*
+function valoresFalsos() {
+    'use strict';
+    
+    var form = document.forms[0];
+    
+    form.id.value = 5;
+    form.nombre.value = 'Javier';
+    form.apellidos.value = 'Lete García';
+    form.email.value = 'javierlete@email.net';
+    form.dni.value = '12345678Z';
+    form['fecha-de-nacimiento'].value = '2000-01-02';
+    form.nacionalidad.value = 1;
+}
+*/
+
+function limpiarFormulario() {
+    'use strict';
+
+    //var form = document.forms[0];
+
+    //$('#nombre').val('');
+    //form.apellidos.value = '';
+
+    //$('input:not([type=hidden])').val('');
+    $('input').val('');
+    $('select').val('0');
+
+    //form.reset();
+}
+
+function extraerCamposUrl(url) {
+    'use strict';
+
+    var datos = [];
+
+    $(url.split('?')[1].split('&')).each(function () {
+        var datoValor = this.split('=');
+        datos[datoValor[0]] = datoValor[1];
+    });
+
+    return datos;
+}
 function anadir() {
     'use strict';
 
     limpiarFormulario();
 
+    //TODO: Quitar esta función cuando esté terminado
+    //valoresFalsos();
+    
     modificarBoton('Añadir', 'primary');
 }
 
@@ -90,46 +157,168 @@ function borrar(id) {
     modificarBoton('Borrar', 'danger');
 }
 
-$(function () {
+function gestionEventoClickHrefOpcion(e, enlace) {
+    'use strict';
+    
+    e.preventDefault();
+
+    var href = extraerCamposUrl(enlace.href);
+
+    console.log(href.opcion, href.id);
+
+    $('#btn-enviar').val(href.opcion);
+
+    switch (href.opcion) {
+    case 'anadir':
+        anadir();
+        break;
+    case 'editar':
+        editar(href.id);
+        break;
+    case 'borrar':
+        borrar(href.id);
+        break;
+    }
+
+    $('#detalle').show();
+}
+
+
+
+
+function formularioAFilaTabla(id) {
     'use strict';
 
+    var celdas, datos, form, fila, f, nac;
+
+    fila = $('#id' + id)[0];
+    datos = fila.dataset;
+    form = $('#formulario')[0];
+    celdas = fila.cells;
+    
+    //TODO: Verificar que el id se recoge correctamente en otros navegadores
+    
+    celdas[1].innerText = datos.nombre = form.nombre.value;
+    celdas[2].innerText = datos.apellidos = form.apellidos.value;
+    celdas[3].innerText = datos.email = form.email.value;
+    celdas[4].innerText = datos.dni = form.dni.value;
+    f = new Date(datos.fechaDeNacimiento = form['fecha-de-nacimiento'].value);
+    datos.nacionalidad = form.nacionalidad.value;
+    
+    celdas[5].innerText =
+        f.getDate() + '/' + (f.getMonth() + 1) + '/' + f.getFullYear();
+    
+    nac = form.nacionalidad;
+    
+    celdas[6].innerText = nac.options[nac.selectedIndex].innerText;
+}
+
+function crearFilaTabla() {
+    'use strict';
+    
+    var celda, n, fila, tbody, id = ++ultimoId;
+    
+    tbody = $('#listado table tbody')[0];
+    
+    fila = tbody.insertRow();
+    
+    fila.id = 'id' + id;
+    
+    fila.insertCell().outerHTML = '<th>' + id + '</th>';
+    
+    for (n = 1; n <= 7; n++) {
+        celda = fila.insertCell();
+    }
+    
+    celda.innerHTML = '<a class="btn btn-primary" href="?opcion=editar&id=' + id + '">Editar</a> <a class="btn btn-danger" href="?opcion=borrar&id=' + id + '">Borrar</a>';
+    
+    $(celda).find('a').click(function (e) {
+        gestionEventoClickHrefOpcion(e, this);
+    });
+    
+    return id;
+}
+
+//TODO: Generar función para id de fila de tabla
+
+function borrarFilaTabla(id) {
+    'use strict';
+    $('#id' + id).remove();
+}
+
+
+$(function () {
+    'use strict';
+    var $form;
+    
+    $form = $('#formulario');
+    
     $('#detalle').hide();
 
     $('a[href*=opcion]').click(function (e) {
-        e.preventDefault();
-
-        var href = extraerCamposUrl(this.href);
-
-        console.log(href.opcion, href.id);
-
-        $('#btn-enviar').val(href.opcion);
-
-        switch (href.opcion) {
-        case 'anadir':
-            anadir();
-            break;
-        case 'editar':
-            editar(href.id);
-            break;
-        case 'borrar':
-            borrar(href.id);
-            break;
-        }
-
-        $('#detalle').show();
+        gestionEventoClickHrefOpcion(e, this);
     });
 
-    $('#formulario').submit(function (e) {
+    $form.submit(function (e) {
+        //TODO: Quitar esto si se va a usar con un servidor normal
         e.preventDefault();
+        e.stopPropagation();
         
-        var opcion, id;
+        var errorFecha, errorDni, $fecha, $dni, $dniError, $fechaError, opcion, id;
+        
+        $fecha = $('#fecha-de-nacimiento');
+        $dni = $('#dni');
+        $dniError = $('#dni-error');
+        $fechaError = $('#fecha-error');
+        
+        if (!esDeEdadLegalParaTrabajar(new Date($fecha.val()))) {
+            errorFecha = 'No tiene edad legal para trabajar. Sólo es válido fechas de nacimiento de personas entre 18 y 65 años';
+        } else {
+            errorFecha = '';
+        }
+        
+        if (!esDniValido($dni.val())) {
+            errorDni = 'La letra de DNI no concuerda con el número';
+        } else if (esDniExtranjero($dni.val()) && $('#nacionalidad').val() !== '2') {
+            errorDni = 'Si el DNI es extranjero la nacionalidad debe ser extranjero';
+        } else {
+            errorDni = '';
+        }
+        
+        $dni[0].setCustomValidity(errorDni);
+        $fecha[0].setCustomValidity(errorFecha);
+        
+        if (!$form[0].checkValidity()) {
+            
+            e.preventDefault();
+            e.stopPropagation();
+            
+            if ($dni[0].validity.patternMismatch) {
+                errorDni = 'El DNI tiene que tener el formato 12345678A';
+            } else if ($dni[0].validity.valueMissing) {
+                errorDni = 'Es obligatorio rellenar el DNI';
+            }
+            
+            if ($fecha[0].validity.valueMissing) {
+                errorFecha = 'Es obligatorio indicar la fecha de nacimiento';
+            }
+            
+            $dniError.text(errorDni);
+            $fechaError.text(errorFecha);
+            
+            $form.addClass('was-validated');
+            
+            return false;
+        }
+        
+        //CORRECTO
         
         opcion = $('#btn-enviar').val();
         id = document.forms[0].id.value;
         
         switch (opcion) {
         case 'anadir':
-            crearUnaFila(id);
+            id = crearFilaTabla();
             formularioAFilaTabla(id);
             
             break;
@@ -147,4 +336,5 @@ $(function () {
         
         $('#detalle').hide();
     });
+    
 });
