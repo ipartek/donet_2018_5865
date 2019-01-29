443fcda9e39d5711edd1582342cf9c0258549c74
Javier Lete <jglete@gmail.com>
2019-01-29 09:08:15 +0100

    Estructuras

---
+++ CSHARP/EjemploPoo/EjemploPoo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPoo
+{
+    class Program
+    {
+        private struct Estructura
+        {
+            public int dato;
+        }
+
+        static void Main(string[] args)
+        {
+            Estructura estructura, estructura2;
+
+            estructura.dato = 1;
+
+            Console.WriteLine(estructura.dato);
+
+            estructura2 = estructura;
+
+            estructura.dato = 7;
+
+            Console.WriteLine(estructura.dato);
+            Console.WriteLine(estructura2.dato);
+
+        }
+    }
+}
