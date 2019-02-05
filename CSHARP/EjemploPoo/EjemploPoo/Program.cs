0921c444a74ac1bce7d1d4598f604f0edba46d5e
Javier Lete <jglete@gmail.com>
2019-02-05 14:07:33 +0100

    Terminal Gui de Miguel de Icaza (responsable de Mono, Xamarin...)

--- CSHARP/EjemploPoo/EjemploPoo/Program.cs
+++ CSHARP/EjemploPoo/EjemploPoo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Terminal.Gui;
 using Tipos;
 using Utilidades;
 
@@ -16,6 +17,46 @@ namespace EjemploPoo
     class Program
     {
         static void Main()
+        {
+            Application.Init();
+            var top = Application.Top;
+
+            // Creates the top-level window to show
+            var win = new Window(new Rect(0, 1, top.Frame.Width, top.Frame.Height - 1), "MyApp");
+            top.Add(win);
+
+            // Creates a menubar, the item "New" has a help menu.
+            var menu = new MenuBar(new MenuBarItem[] {
+                new MenuBarItem ("_File", new MenuItem [] {
+                    new MenuItem ("_New", "Creates new file", null),
+                    new MenuItem ("_Close", "", null),
+                    new MenuItem ("_Quit", "", null)
+                }),
+                new MenuBarItem ("_Edit", new MenuItem [] {
+                    new MenuItem ("_Copy", "", null),
+                    new MenuItem ("C_ut", "", null),
+                    new MenuItem ("_Paste", "", null)
+                })
+            });
+
+            top.Add(menu);
+
+            // Add some controls
+            win.Add(
+                    new Label(3, 2, "Login: "),
+                    new TextField(14, 2, top.Frame.Width - 17, ""),
+                    new Label(3, 4, "Password: "),
+                    new TextField(14, 4, 40, "") { Secret = true },
+                    new CheckBox(3, 6, "Remember me"),
+                    new RadioGroup(3, 8, new[] { "_Personal", "_Company" }),
+                    new Button(3, 14, "Ok"),
+                    new Button(10, 14, "Cancel"),
+                    new Label(3, 18, "Press ESC and 9 to activate the menubar"));
+
+            Application.Run();
+        }
+
+        static void MainValorPorDefectoReadLine()
         {
             Write("Dime tu nombre: ");
             System.Windows.Forms.SendKeys.SendWait("Javier");
@@ -55,7 +96,7 @@ namespace EjemploPoo
                 Console.WriteLine($"{par.Key} = {par.Value}");
             }
 
-            foreach(string clave in dic.Keys)
+            foreach (string clave in dic.Keys)
             {
                 Console.WriteLine(clave);
             }
@@ -210,7 +251,7 @@ namespace EjemploPoo
                 Console.WriteLine(oe.Message);
                 throw new Exception("Error de rebose de variable", oe);
             }
-            catch(TiposException te)
+            catch (TiposException te)
             {
                 Console.WriteLine(te.Message);
             }
