0b77a6d51c9c208cb9a17de3dbd56ea53158b7bd
Javier Lete <jglete@gmail.com>
2019-02-28 13:22:33 +0100

    Visualización de datos con GridView

---
+++ CSHARP/EjemploBBDD/PresentacionWeb/Usuarios.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Usuarios.aspx.cs" Inherits="PresentacionWeb.Usuarios" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:GridView CssClass="table" ID="GvUsuarios" runat="server"></asp:GridView>
+    <asp:Button runat="server" Text="Refrescar"/>
+</asp:Content>
---
+++ CSHARP/EjemploBBDD/PresentacionWeb/Usuarios.aspx.cs
@@ -0,0 +1,26 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Tipos;
+
+namespace PresentacionWeb
+{
+    public partial class Usuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cadenaConexion = @"Data Source=DESKTOP-9GKENR5\SQLEXPRESS;Initial Catalog=ipartek;Integrated Security=True";
+            IDao<Usuario> usuariosDao = new UsuarioSqlDao(cadenaConexion);
+
+            if (!IsPostBack)
+            {
+                GvUsuarios.DataSource = usuariosDao.BuscarTodos();
+                GvUsuarios.DataBind();
+            }
+        }
+    }
+}
