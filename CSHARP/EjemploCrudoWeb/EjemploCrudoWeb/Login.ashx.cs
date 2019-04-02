8d51e6ac496074e72ec3dbc5263a2e012da748fc
Javier Lete <jglete@gmail.com>
2019-04-02 12:29:12 +0200

    Ejemplo de login muy básico de MVC sin frameworks

---
+++ CSHARP/EjemploCrudoWeb/EjemploCrudoWeb/Login.ashx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EjemploCrudoWeb
+{
+    /// <summary>
+    /// Descripción breve de Login
+    /// </summary>
+    public class Login : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "texto/html";
+
+            Usuario usuario = new Usuario()
+            {
+                Email = context.Request["email"],
+                Password = context.Request["password"]
+            };
+
+            if(usuario.Email == "javierlete@email.net" && usuario.Password == "contra")
+            {
+                context.Response.Redirect("Principal.htm");
+            }
+            else
+            {
+                context.Response.Redirect("Default.htm");
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
