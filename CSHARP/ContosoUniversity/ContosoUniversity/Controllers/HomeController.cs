50070458d83e94c074e208a7809e8772b0edc2c6
Javier Lete <jglete@gmail.com>
2019-04-02 09:13:50 +0200

    Database.SqlQuery

--- CSHARP/ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
+++ CSHARP/ContosoUniversity/ContosoUniversity/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using ContosoUniversity.DAL;
 using ContosoUniversity.ViewModels;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -16,13 +17,22 @@ namespace ContosoUniversity.Controllers
 
         public ActionResult About()
         {
-            var data = from student in db.Students
-                        group student by student.EnrollmentDate into dateGroup
-                        select new EnrollmentDateGroup()
-                        {
-                            EnrollmentDate = dateGroup.Key,
-                            StudentCount = dateGroup.Count()
-                        };
+            // Commenting out LINQ to show how to do the same thing in SQL.
+            //IQueryable<EnrollmentDateGroup> = from student in db.Students
+            //           group student by student.EnrollmentDate into dateGroup
+            //           select new EnrollmentDateGroup()
+            //           {
+            //               EnrollmentDate = dateGroup.Key,
+            //               StudentCount = dateGroup.Count()
+            //           };
+
+            // SQL version of the above LINQ code.
+            string query = "SELECT EnrollmentDate, COUNT(*) AS StudentCount "
+                + "FROM Person "
+                + "WHERE Discriminator = 'Student' "
+                + "GROUP BY EnrollmentDate";
+            IEnumerable<EnrollmentDateGroup> data = db.Database.SqlQuery<EnrollmentDateGroup>(query);
+
             return View(data.ToList());
         }
 
