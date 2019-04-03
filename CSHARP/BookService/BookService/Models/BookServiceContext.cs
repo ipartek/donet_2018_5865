8715716d93bb0eb6e093bca4c0c41db774b80a48
Javier Lete <jglete@gmail.com>
2019-04-03 13:37:09 +0200

    Migraciones iniciales

--- CSHARP/BookService/BookService/Models/BookServiceContext.cs
+++ CSHARP/BookService/BookService/Models/BookServiceContext.cs
@@ -19,6 +19,8 @@ namespace BookService.Models
         {
         }
 
-        public System.Data.Entity.DbSet<BookService.Models.Author> Authors { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
     }
 }
