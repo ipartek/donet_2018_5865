81a327b6c60117f01b50386cd42b4073897d3eb3
Javier Lete <jglete@gmail.com>
2019-04-15 10:38:35 +0200

    Modelo para usuario

---
+++ CSHARP/RepasoMVC/RepasoMVC/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RepasoMVC.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Correo Electrónico")]
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
