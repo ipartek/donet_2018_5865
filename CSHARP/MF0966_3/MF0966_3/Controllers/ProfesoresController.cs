d936580eb2277acdec67575dd414b2514dff885f
Javier Lete <jglete@gmail.com>
2019-04-16 01:27:41 +0200

    Alumnos, Clientes y Profesores

---
+++ CSHARP/MF0966_3/MF0966_3/Controllers/ProfesoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MF0966_3.Models;
+
+namespace MF0966_3.Controllers
+{
+    public class ProfesoresController : Controller
+    {
+        private UF2215Entities db = new UF2215Entities();
+
+        // GET: Profesores
+        public ActionResult Index()
+        {
+            return View(db.Profesores.ToList());
+        }
+
+        // GET: Profesores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = db.Profesores.Find(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // GET: Profesores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profesores/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,NumeroSeguridadSocial,Nombre,Apellidos,FechaNacimiento,Dni,Direccion,Poblacion,CodigoPostal,Telefono,Email,Activo")] Profesor profesor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Profesores.Add(profesor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(profesor);
+        }
+
+        // GET: Profesores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = db.Profesores.Find(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // POST: Profesores/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,NumeroSeguridadSocial,Nombre,Apellidos,FechaNacimiento,Dni,Direccion,Poblacion,CodigoPostal,Telefono,Email,Activo")] Profesor profesor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(profesor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(profesor);
+        }
+
+        // GET: Profesores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profesor profesor = db.Profesores.Find(id);
+            if (profesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profesor);
+        }
+
+        // POST: Profesores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Profesor profesor = db.Profesores.Find(id);
+            db.Profesores.Remove(profesor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
