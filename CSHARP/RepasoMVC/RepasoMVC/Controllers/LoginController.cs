7e3910a1c12cc6615f5d0ac254ebd6cbb236e288
Javier Lete <jglete@gmail.com>
2019-04-15 10:53:39 +0200

    Acciones diferentes para Post y para Get

--- CSHARP/RepasoMVC/RepasoMVC/Controllers/LoginController.cs
+++ CSHARP/RepasoMVC/RepasoMVC/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RepasoMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,22 @@ namespace RepasoMVC.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(usuario);
         }
     }
 }
