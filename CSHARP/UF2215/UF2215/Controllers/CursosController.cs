b49827849e330f56a6afe99e3a90a25d9f84b016
Javier Lete <jglete@gmail.com>
2019-04-16 18:09:45 +0200

    Obtener cursos

---
+++ CSHARP/UF2215/UF2215/Controllers/CursosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using UF2215.Models;
+
+namespace UF2215.Controllers
+{
+    public class CursosController : ApiController
+    {
+        private UF2215Entities ctx = new UF2215Entities();
+
+        public IQueryable GetCursos()
+        {
+            return ctx.Cursos.Select(c => new { c.Nombre, c.Identificador, c.NumeroHoras });
+        }
+    }
+}
