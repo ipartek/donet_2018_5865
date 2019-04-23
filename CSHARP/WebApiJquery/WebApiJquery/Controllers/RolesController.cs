7e3cdd8e8b691920cb4d14984b8b089d14ed469d
Javier Lete <jglete@gmail.com>
2019-04-23 09:14:16 +0200

    WebAPI + index.html de plantilla

---
+++ CSHARP/WebApiJquery/WebApiJquery/Controllers/RolesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiJquery.Models;
+
+namespace WebApiJquery.Controllers
+{
+    public class RolesController : ApiController
+    {
+        private IpartekEntities db = new IpartekEntities();
+
+        // GET: api/Roles
+        public IQueryable<Rol> GetRols()
+        {
+            return db.Rols;
+        }
+
+        // GET: api/Roles/5
+        [ResponseType(typeof(Rol))]
+        public async Task<IHttpActionResult> GetRol(int id)
+        {
+            Rol rol = await db.Rols.FindAsync(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rol);
+        }
+
+        // PUT: api/Roles/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutRol(int id, Rol rol)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != rol.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(rol).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RolExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Roles
+        [ResponseType(typeof(Rol))]
+        public async Task<IHttpActionResult> PostRol(Rol rol)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Rols.Add(rol);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = rol.Id }, rol);
+        }
+
+        // DELETE: api/Roles/5
+        [ResponseType(typeof(Rol))]
+        public async Task<IHttpActionResult> DeleteRol(int id)
+        {
+            Rol rol = await db.Rols.FindAsync(id);
+            if (rol == null)
+            {
+                return NotFound();
+            }
+
+            db.Rols.Remove(rol);
+            await db.SaveChangesAsync();
+
+            return Ok(rol);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RolExists(int id)
+        {
+            return db.Rols.Count(e => e.Id == id) > 0;
+        }
+    }
+}
