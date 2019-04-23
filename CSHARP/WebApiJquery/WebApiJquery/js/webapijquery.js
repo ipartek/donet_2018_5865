a238637c9768c4943b23c5062f88b523d36e7513
Javier Lete <jglete@gmail.com>
2019-04-23 09:33:53 +0200

    Plantilla completa

--- CSHARP/WebApiJquery/WebApiJquery/js/webapijquery.js
+++ CSHARP/WebApiJquery/WebApiJquery/js/webapijquery.js
@@ -5,7 +5,15 @@ $(function () {
         console.log(roles);
 
         $(roles).each(function () {
-            var fila = `<tr><th>${this.Id}</th><td>${this.Nombre}</td></tr>`;
+            var fila = `
+                <tr>
+                    <th>${this.Id}</th>
+                    <td>${this.Nombre}</td>
+                    <td>
+                        <a class="update" data-id="${this.Id}" href="#">Editar</a>
+                        <a class="delete" data-id="${this.Id}" href="#">Borrar</a>
+                    </td>
+                </tr>`;
             $(fila).appendTo('tbody');
         });
     });
