7c820a969630ac7bee13d5e08dc50419e4235d1c
Javier Lete <jglete@gmail.com>
2019-04-23 09:28:01 +0200

    Listado de roles en pantalla

---
+++ CSHARP/WebApiJquery/WebApiJquery/js/webapijquery.js
@@ -0,0 +1,12 @@
+﻿var url = 'api/roles/';
+
+$(function () {
+    $.getJSON(url, function (roles) {
+        console.log(roles);
+
+        $(roles).each(function () {
+            var fila = `<tr><th>${this.Id}</th><td>${this.Nombre}</td></tr>`;
+            $(fila).appendTo('tbody');
+        });
+    });
+});
