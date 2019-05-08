7c874a7e50e8f562041160dee2642171870ee031
Javier Lete <jglete@gmail.com>
2019-05-08 14:12:01 +0200

    guardarNaves en lugar de guardarDatos

--- UF1643/index.js
+++ UF1643/index.js
@@ -1,18 +1,18 @@
 var naves = [];
 
 $(function () {
-    guardarDatos('https://swapi.co/api/starships/').then(function () {
+    guardarNaves('https://swapi.co/api/starships/').then(function () {
         console.log(naves);
     });
 });
 
-function guardarDatos(url) {
+function guardarNaves(url) {
     var promesa = $.getJSON(url).then(function(datos) {
         if (datos.results) {
             naves.push(...datos.results);
 
             if (datos.next) {
-                return guardarDatos(datos.next);
+                return guardarNaves(datos.next);
             }
         }
     });
