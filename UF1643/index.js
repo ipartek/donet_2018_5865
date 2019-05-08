cecd22902ce0296fb0bd2f5646c9d71c2c688cee
Javier Lete <jglete@gmail.com>
2019-05-08 14:46:14 +0200

    Obtención de datos de piloto mediante promesas

--- UF1643/index.js
+++ UF1643/index.js
@@ -1,9 +1,20 @@
+var piloto;
+
+var api = 'https://swapi.co/api/';
+
 var naves = [];
 
 $(function () {
-    guardarNaves('https://swapi.co/api/starships/').then(function () {
+    guardarNaves(api + 'starships').then(function () {
         console.log(naves);
     });
+
+    obtenerPiloto().then(obtenerPlaneta).then(obtenerEspecie).then(
+        function(pilot){
+            piloto = pilot.piloto;
+            console.log(piloto);
+        });
+
 });
 
 function guardarNaves(url) {
@@ -17,5 +28,36 @@ function guardarNaves(url) {
         }
     });
 
+    return promesa;
+}
+
+function obtenerPiloto()
+{
+    var promesa = $.getJSON(api + 'people/14').then(function (pilot) {
+        pilot.piloto = {};
+        pilot.piloto.nombre = pilot.name;
+        return pilot;
+    });
+
+    return promesa;
+}
+
+function obtenerPlaneta(pilot)
+{
+    var promesa = $.getJSON(pilot.homeworld).then(function(planeta) {
+        pilot.piloto.planeta = planeta.name;
+        return pilot;
+    });
+
+    return promesa;
+}
+
+function obtenerEspecie(pilot)
+{
+    var promesa = $.getJSON(pilot.species[0]).then(function(especie) {
+        pilot.piloto.especie = especie.name;
+        return pilot;
+    });
+    
     return promesa;
 }
