81d22c3d62441110df79c842011b343b114d3b00
Javier Lete <jglete@gmail.com>
2019-05-09 16:21:15 +0200

    Control de errores de planetas y/o especies

--- UF1643/index.js
+++ UF1643/index.js
@@ -1,29 +1,33 @@
 var piloto;
 
-var api = 'https://swapi.co/api/';
+var api = 'http://192.168.0.77:8000/api/'; //'https://swapi.co/api/';
 
 var naves = [];
 
 $(function () {
     guardarNaves(api + 'starships').then(function () {
         console.log(naves);
-    }).then(function() {
-        $(naves[3].films).each(function() {
-            obtenerPelicula(this).then(function(pelicula) {
+    }).then(function () {
+        $(naves[4].films).each(function () {
+            obtenerPelicula(this).then(function (pelicula) {
                 console.log('Película', pelicula.title, pelicula.episode_id);
             });
         });
-        $(naves[3].pilots).each(function () {
+        $(naves[4].pilots).each(function () {
             //obtenerPiloto().then(obtenerPlaneta).then(obtenerEspecie).then(
             obtenerPiloto(this).then(function (pilot) {
-                return $.when(obtenerPlaneta(pilot), obtenerEspecie(pilot));
+                return $.when(obtenerPlaneta(pilot), obtenerEspecie(pilot)).fail(function () {
+                    piloto = pilot.piloto;
+                    console.error('Piloto sin especie o planeta', piloto);
+                });
             }).then(
                 function (pilot) {
                     piloto = pilot.piloto;
                     console.log('Piloto', piloto);
+                    return pilot;
                 });
         });
-    
+
     });
 
 
@@ -32,7 +36,7 @@ $(function () {
 
 function obtenerPelicula(url) {
     var promesa = $.getJSON(url);
-    
+
     return promesa;
 }
 function guardarNaves(url) {
@@ -60,18 +64,32 @@ function obtenerPiloto(pilotoUrl) {
 }
 
 function obtenerPlaneta(pilot) {
+    if(!pilot.homeworld) {
+        console.warn(`El piloto ${pilot.name} no tiene planeta`);
+        return pilot;
+    }
+    
     var promesa = $.getJSON(pilot.homeworld).then(function (planeta) {
         pilot.piloto.planeta = planeta.name;
         return pilot;
+    }).fail(function () {
+        console.error(`El piloto ${pilot.name} no tiene planeta`);
     });
 
     return promesa;
 }
 
 function obtenerEspecie(pilot) {
+    if(!pilot.species[0]){
+        console.warn(`El piloto ${pilot.name} no tiene especie`);
+        return pilot;
+    }
+
     var promesa = $.getJSON(pilot.species[0]).then(function (especie) {
         pilot.piloto.especie = especie.name;
         return pilot;
+    }).fail(function () {
+        console.error(`El piloto ${pilot.name} no tiene especie`);
     });
 
     return promesa;
